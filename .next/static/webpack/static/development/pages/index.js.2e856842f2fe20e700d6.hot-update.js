88a4f39f564a0009b213f707cc9b9f8df0b84399
xaviguasch <xaviguasch@gmail.com>
2019-06-04 15:45:34 +0200

    added first component in pages directory

---
+++ .next/static/webpack/static/development/pages/index.js.2e856842f2fe20e700d6.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/xaviguasch/dev/courses-sept-2018/steele-the-modern-react-bootcamp/s37-next-js/01-first-next-app/first-next/pages/index.js";
+
+
+var Index = function Index() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "our Index page!!!"));
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.2e856842f2fe20e700d6.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>our Index page!!!</h1>
+    </div>
+  )
+}
+
+export default Index
